248ab8f0061352b1cc3e017e85d1be8608693972
TekkifyLife <tekkifylife@outlook.com>
2013-07-12 03:16:27 -0700

    2.0.1 cleanup release

--- src/com/tekkifylife/reimonkey/Strings.java
+++ src/com/tekkifylife/reimonkey/Strings.java
@@ -2,6 +2,5 @@ package com.tekkifylife.reimonkey;
 
 public class Strings {
 
-	public static final String REIMONKEY_SUCCESS = "[ReiMonkey] ReiMonkey injected into Rei's Minimap.";
 	public static final String REIS_MINIMAP_MISSING = "\u00A77[\u00A76ReiMonkey\u00A77]\u00A7r Rei's Minimap is not installed. Download it here: http://www.minecraftforum.net/topic/482147-162-jul08-reis-minimap-v34-01/";
 }
